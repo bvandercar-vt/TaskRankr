be0fd65e88c7b73656312be819caca55f1b7a4d9
Blake V. <87083504+bvandercar-vt@users.noreply.github.com>
2026-03-06 22:33:43 +0000

    format

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -13,98 +13,121 @@
 
 // If the loader is already loaded, just stop.
 if (!self.define) {
-  let registry = {};
+  const registry = {}
 
   // Used for `eval` and `importScripts` where we can't get script URL by other means.
   // In both cases, it's safe to use a global var because those functions are synchronous.
-  let nextDefineUri;
+  let nextDefineUri
 
   const singleRequire = (uri, parentUri) => {
-    uri = new URL(uri + ".js", parentUri).href;
-    return registry[uri] || (
-      
-        new Promise(resolve => {
-          if ("document" in self) {
-            const script = document.createElement("script");
-            script.src = uri;
-            script.onload = resolve;
-            document.head.appendChild(script);
-          } else {
-            nextDefineUri = uri;
-            importScripts(uri);
-            resolve();
-          }
-        })
-      
-      .then(() => {
-        let promise = registry[uri];
-        if (!promise) {
-          throw new Error(`Module ${uri} didn’t register its module`);
+    uri = new URL(uri + '.js', parentUri).href
+    return (
+      registry[uri] ||
+      new Promise((resolve) => {
+        if ('document' in self) {
+          const script = document.createElement('script')
+          script.src = uri
+          script.onload = resolve
+          document.head.appendChild(script)
+        } else {
+          nextDefineUri = uri
+          importScripts(uri)
+          resolve()
         }
-        return promise;
+      }).then(() => {
+        const promise = registry[uri]
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`)
+        }
+        return promise
       })
-    );
-  };
+    )
+  }
 
   self.define = (depsNames, factory) => {
-    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    const uri =
+      nextDefineUri ||
+      ('document' in self ? document.currentScript.src : '') ||
+      location.href
     if (registry[uri]) {
       // Module is already loading or loaded.
-      return;
+      return
     }
-    let exports = {};
-    const require = depUri => singleRequire(depUri, uri);
+    const exports = {}
+    const require = (depUri) => singleRequire(depUri, uri)
     const specialDeps = {
       module: { uri },
       exports,
-      require
-    };
-    registry[uri] = Promise.all(depsNames.map(
-      depName => specialDeps[depName] || require(depName)
-    )).then(deps => {
-      factory(...deps);
-      return exports;
-    });
-  };
-}
-define(['./workbox-959db468'], (function (workbox) { 'use strict';
-
-  self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-      self.skipWaiting();
+      require,
     }
-  });
+    registry[uri] = Promise.all(
+      depsNames.map((depName) => specialDeps[depName] || require(depName)),
+    ).then((deps) => {
+      factory(...deps)
+      return exports
+    })
+  }
+}
+define(['./workbox-959db468'], (workbox) => {
+  self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+      self.skipWaiting()
+    }
+  })
 
   /**
    * The precacheAndRoute() method efficiently caches and responds to
    * requests for URLs in the manifest.
    * See https://goo.gl/S9QRab
    */
-  workbox.precacheAndRoute([{
-    "url": "/index.html",
-    "revision": "0.49chrasombo"
-  }], {});
-  workbox.cleanupOutdatedCaches();
-  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
-    allowlist: [/^\/$/]
-  }));
-  workbox.registerRoute(/^https:\/\/fonts\.googleapis\.com\/.*/i, new workbox.CacheFirst({
-    "cacheName": "google-fonts-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 31536000
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-  workbox.registerRoute(/^https:\/\/fonts\.gstatic\.com\/.*/i, new workbox.CacheFirst({
-    "cacheName": "gstatic-fonts-cache",
-    plugins: [new workbox.ExpirationPlugin({
-      maxEntries: 10,
-      maxAgeSeconds: 31536000
-    }), new workbox.CacheableResponsePlugin({
-      statuses: [0, 200]
-    })]
-  }), 'GET');
-
-}));
+  workbox.precacheAndRoute(
+    [
+      {
+        url: '/index.html',
+        revision: '0.49chrasombo',
+      },
+    ],
+    {},
+  )
+  workbox.cleanupOutdatedCaches()
+  workbox.registerRoute(
+    new workbox.NavigationRoute(
+      workbox.createHandlerBoundToURL('/index.html'),
+      {
+        allowlist: [/^\/$/],
+      },
+    ),
+  )
+  workbox.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com\/.*/i,
+    new workbox.CacheFirst({
+      cacheName: 'google-fonts-cache',
+      plugins: [
+        new workbox.ExpirationPlugin({
+          maxEntries: 10,
+          maxAgeSeconds: 31_536_000,
+        }),
+        new workbox.CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    }),
+    'GET',
+  )
+  workbox.registerRoute(
+    /^https:\/\/fonts\.gstatic\.com\/.*/i,
+    new workbox.CacheFirst({
+      cacheName: 'gstatic-fonts-cache',
+      plugins: [
+        new workbox.ExpirationPlugin({
+          maxEntries: 10,
+          maxAgeSeconds: 31_536_000,
+        }),
+        new workbox.CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+      ],
+    }),
+    'GET',
+  )
+})
