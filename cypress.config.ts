ccd73c4da6e676ade75a69bb962a21ad7fa5e9a4
Blake Vandercar <blake.vandercar@validatek.com>
2026-03-11 19:41:07 -0600

    cypress-vite

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'cypress'
+import vitePreprocessor from 'cypress-vite'
 
 export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: true,
+  fixturesFolder: false,
   e2e: {
     baseUrl: 'http://localhost:5000',
-    supportFile: 'cypress/support/e2e.ts',
     specPattern: 'cypress/e2e/**/*.cy.ts',
-    video: false,
-    screenshotOnRunFailure: true,
+    setupNodeEvents(on) {
+      on('file:preprocessor', vitePreprocessor())
+    },
   },
 })
