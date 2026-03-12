c917a333691db4bc1d1e24f6a4b21348ab3fa9d9
Blake Vandercar <blake.vandercar@validatek.com>
2026-03-12 12:51:12 -0600

    be.enabled

--- cypress/support/utils/task-form.ts
+++ cypress/support/utils/task-form.ts
@@ -23,7 +23,7 @@ export const fillTaskForm = (
   )
 
   cy.get(TaskForm.SUBMIT_BTN) //
-    .should(requiredFields.length ? 'be.disabled' : 'not.be.disabled')
+    .should(requiredFields.length ? 'be.disabled' : 'be.enabled')
 
   const filled = new Set<RankField>()
   for (const field of rankFields) {
@@ -38,7 +38,7 @@ export const fillTaskForm = (
       }
       const allRequiredFilled = requiredFields.every((f) => filled.has(f))
       cy.get(TaskForm.SUBMIT_BTN) //
-        .should(allRequiredFilled ? 'not.be.disabled' : 'be.disabled')
+        .should(allRequiredFilled ? 'be.enabled' : 'be.disabled')
     } else {
       cy.get(RankSelect).should('not.exist')
     }
@@ -46,6 +46,6 @@ export const fillTaskForm = (
 
   cy.get(TaskForm.SUBMIT_BTN)
     .should('have.text', submitBtnText)
-    .should('not.be.disabled')
+    .should('be.enabled')
     .click()
 }
