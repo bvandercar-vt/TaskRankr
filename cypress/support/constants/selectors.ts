1543e4f9936c01e599e103bf61ab269f9ab06af0
Blake V. <87083504+bvandercar-vt@users.noreply.github.com>
2026-04-13 23:47:45 -0600

    test(e2e): time progress required (#61)

--- cypress/support/constants/selectors.ts
+++ cypress/support/constants/selectors.ts
@@ -29,6 +29,9 @@ export const Selectors = {
   TaskForm: {
     NAME_INPUT: testId('task-name-input'),
     TIME_SPENT_INPUT: testId('time-spent-input'),
+    TIME_SPENT_INPUT_HOURS: testId('time-spent-input-hours'),
+    TIME_SPENT_INPUT_MINUTES: testId('time-spent-input-minutes'),
+    MARK_COMPLETED_CHECKBOX: testId('mark-completed-checkbox'),
     rankSelect: (field: RankField) => testId(`rank-select-${field}`),
     SUBMIT_BTN: testId('submit-button'),
     CANCEL_BTN: testId('cancel-button'),
