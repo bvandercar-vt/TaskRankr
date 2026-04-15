badedbfc181013ba1847e2b988d9fb37ef09a988
Blake Vandercar <blake.vandercar@validatek.com>
2026-04-14 21:59:10 -0600

    attempt

--- cypress/support/utils/task-tree.ts
+++ cypress/support/utils/task-tree.ts
@@ -6,13 +6,26 @@ const { TaskCard } = Selectors
 type TaskTreeNode = Pick<Task, 'name'> & { subtasks?: TaskTreeNode[] }
 
 const checkTitleAndSubtasks = (task: TaskTreeNode) => {
-  const cardTitle = cy
-    .contains(`${TaskCard.CARD} ${TaskCard.TITLE}`, new RegExp(`^${task.name}$`))
-    .should('exist')
+  const cardTitle = () =>
+    cy
+      .contains(
+        `${TaskCard.CARD} ${TaskCard.TITLE}`,
+        new RegExp(`^${task.name}$`),
+      )
+      .should('exist')
+
+  cardTitle().should('exist').closest(TaskCard.CARD).should('exist')
+
   if (!task.subtasks?.length) return
-  cardTitle.closest(TaskCard.CARD).find(TaskCard.EXPAND_BTN).click()
-  cardTitle.closest(TaskCard.CARD).find(TaskCard.EXPAND_BTN).should('not.exist')
-  cardTitle.closest(TaskCard.CARD).within(() => checkSubtasksInCard(task))
+
+  cardTitle().closest(TaskCard.CARD).find(TaskCard.EXPAND_BTN).click()
+  cardTitle()
+    .closest(TaskCard.CARD)
+    .find(TaskCard.EXPAND_BTN)
+    .should('not.exist')
+  cardTitle()
+    .closest(TaskCard.CARD)
+    .within(() => checkSubtasksInCard(task))
 }
 
 const checkSubtasksInCard = (task: TaskTreeNode) => {
