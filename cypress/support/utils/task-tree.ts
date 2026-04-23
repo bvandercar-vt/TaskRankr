280f3a74e9e31ecf4e0843466779a59abbcd631e
Blake Vandercar <blake.vandercar@validatek.com>
2026-04-23 15:19:08 -0600

    back to 100

--- cypress/support/utils/task-tree.ts
+++ cypress/support/utils/task-tree.ts
@@ -19,6 +19,7 @@ export const getTaskCardTitle = (task: Pick<Task, 'name'>) =>
     .should('be.visible')
 
 const checkTitleAndSubtasks = (task: TaskTreeNode, tier: number) => {
+  cy.wait(100) // TODO: debug - if flakes, increase to 300
   const getTaskCard = () =>
     getTaskCardTitle(task)
       .should(
